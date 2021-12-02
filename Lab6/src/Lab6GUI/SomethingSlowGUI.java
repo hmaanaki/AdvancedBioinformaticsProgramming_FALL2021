15522351e17cf43909aa64874758b179699a5eb1
hmaanaki <61711664+hmaanaki@users.noreply.github.com>
2021-12-01 21:51:23 -0500

    Add files via upload

--- Lab6/src/Lab6GUI/SomethingSlowGUI.java
+++ Lab6/src/Lab6GUI/SomethingSlowGUI.java
@@ -2,8 +2,12 @@ package Lab6GUI;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.SwingWorker;
 
 import lab6.SomethingSlow;
 
@@ -11,26 +15,26 @@ import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
+import java.util.List;
 import java.awt.BorderLayout;
 
 public class SomethingSlowGUI extends JFrame{
-	
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 1L;
-	private JButton start_button = new JButton("Start");
-	private JButton cancel_button = new JButton("Cancel");
+	private final JButton start_button = new JButton("Start");
+	private final JButton cancel_button = new JButton("Cancel");
 	
+	private final JLabel update_window = new JLabel();
+	private final JTextArea list_of_primes_window = new JTextArea();
 	
 	public SomethingSlowGUI() {
-		super("Lets Factor!");
-		setSize(300,500);
+		super("Prime Time!");
+		setSize(400,500);
 		setLocationRelativeTo(null);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
 		getContentPane().setLayout(new BorderLayout());
 		getContentPane().add(bottomLayout(), BorderLayout.SOUTH);
+		getContentPane().add(centerLayout(), BorderLayout.CENTER);
 		
 		setVisible(true);	
 	}
@@ -45,6 +49,9 @@ public class SomethingSlowGUI extends JFrame{
 		start_button.addActionListener(
 				new ActionListener() {
 					public void actionPerformed(ActionEvent e) {	
+						update_window.setText("");
+						list_of_primes_window.setText("");
+						
 						ArrayList<Object> thread_num_array = new ArrayList<Object>();
 						for(int i =1; i<7; i++) {
 							thread_num_array.add(i);
@@ -53,7 +60,8 @@ public class SomethingSlowGUI extends JFrame{
 						String prime_number_input = (String)JOptionPane.showInputDialog(null, "Enter a number you wish to find the primes for: ", "User Input Window", JOptionPane.PLAIN_MESSAGE, null, null, null);
 						start_button.setEnabled(false);
 						try {
-							SomethingSlow.call_main(thread_options,Long.parseLong(prime_number_input));
+							new UpdatePrimesFound().execute();
+							SomethingSlow.call_main(thread_options,Integer.valueOf(prime_number_input));
 						} catch (NumberFormatException e1) {
 							e1.printStackTrace();
 						} catch (Exception e1) {
@@ -65,6 +73,41 @@ public class SomethingSlowGUI extends JFrame{
 		return south_buttons;
 	}
 	
+	private JPanel centerLayout() {
+		JPanel center_layout = new JPanel();
+		center_layout.setLayout(new GridLayout(2,2));
+		center_layout.add(update_window);
+		JScrollPane list_of_primes_scroll = new JScrollPane (list_of_primes_window, JScrollPane.VERTICAL_SCROLLBAR_ALWAYS,JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+		center_layout.add(list_of_primes_scroll);
+		
+		return center_layout;
+	}
+	
+	private class UpdatePrimesFound extends SwingWorker<Void, String>{
+		@Override
+		protected Void doInBackground() throws Exception {
+			int i = 0;
+			while(SomethingSlow.calculation_completed()) {
+				System.out.println("Hello");
+				i+=1;
+				publish(String.valueOf(i));
+				Thread.sleep(1000);
+			}
+			list_of_primes_window.append("Here are the primes that were found: \n");
+			List<Integer> list_of_primes = SomethingSlow.get_list_of_primes();
+			for(Integer val: list_of_primes) {
+				list_of_primes_window.append(Integer.toString(val)+ "\n");
+			}
+			start_button.setEnabled(true);
+			return null;
+		}
+		public void process(List<String> time_ellapsed) {
+			for(String s: time_ellapsed) {
+				update_window.setText("Ellapsed Time: " + String.valueOf(s) + 
+						" sec | Number of Primes Found: " + SomethingSlow.number_primes_found());
+			}
+		}
+	}
 	public static void main(String Args[]) {
 		new SomethingSlowGUI();
 	}
--- Lab6/src/lab6/SomethingSlow.java
+++ Lab6/src/lab6/SomethingSlow.java
@@ -1,31 +1,61 @@
 package lab6;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
 import java.util.concurrent.Semaphore;
+import java.util.concurrent.TimeUnit;
 
 public class SomethingSlow implements Runnable{
 	
 	static int num_threads;
-	private static long value_to_factor = 0;
+	private static int value_to_factor = 0;
 	private final Semaphore semaphore;
+	private static int min_val = 0;
+	private static int max_val = 0;
+	private volatile static boolean completed = true;
 	
-	public SomethingSlow(Semaphore semaphore, long value_to_factor) {
-		this.value_to_factor = value_to_factor;
+	private static final List<Integer> list_of_primes = Collections.synchronizedList(new ArrayList<Integer>());
+	
+	public SomethingSlow(Semaphore semaphore, int min_val, int max_val) {
+		SomethingSlow.min_val = min_val;
+		SomethingSlow.max_val = max_val;
 		this.semaphore = semaphore;
 	}
 	
-	static void find_prime_numbers(long value_to_factor) {
-		
-		long half = value_to_factor/2 +1;	
-		for(long i=2; i<half; i++) {
-			if(value_to_factor % i == 0) {
-				System.out.println(i);
+	static void find_prime_numbers(int min_val, int max_val) {
+		for(int i=min_val; i<max_val; i++) {
+			Boolean prime = true;
+			for(int j=2; j < i; j++) {
+				if(i % j == 0) {
+					prime = false;
+				}
+			}
+			if(prime & i != 0 & i != 1) {
+				synchronized (list_of_primes) {
+					list_of_primes.add(i);
+				}
 			}
 		}
 	}
 	
+	public static int number_primes_found() {
+		return list_of_primes.size();
+	}
+	
+	public static boolean calculation_completed(){
+		return completed;
+	}
+	
+	public static List<Integer> get_list_of_primes(){
+		return list_of_primes;
+	}
+	
 	public void run() {
 		try {
-			find_prime_numbers(value_to_factor);
+			find_prime_numbers(min_val, max_val);
 		}
 		catch(Exception e) {
 			System.out.println("Oh no.");
@@ -35,7 +65,7 @@ public class SomethingSlow implements Runnable{
 		}
 	}
 	
-	public static void call_main(int num_threads, long value_to_factor) throws Exception {
+	public static void call_main(int num_threads, int value_to_factor) throws Exception {
 		SomethingSlow.value_to_factor = value_to_factor;
 		SomethingSlow.num_threads = num_threads;
 		main(null);
@@ -44,17 +74,34 @@ public class SomethingSlow implements Runnable{
 	public static void main(String[] Args) throws Exception{
 		
 		long start_time = System.currentTimeMillis();
+		int interval_spacing = value_to_factor / num_threads;
+		
 		Semaphore semaphore = new Semaphore(num_threads);
+		
+		ConcurrentMap<Integer, List<Integer>> intervals = new ConcurrentHashMap<Integer, List<Integer>>();
+		for(int i =0; i < num_threads; i++) {
+			min_val = max_val;
+			max_val = min_val + interval_spacing;
+			List<Integer> interval = new ArrayList<Integer>();
+			interval.add(min_val);
+			interval.add(max_val);
+			intervals.put(i+1, interval );
+		}
+		
 		for(int i = 0; i < num_threads; i++) {
-			semaphore.acquire();
-			SomethingSlow worker = new SomethingSlow(semaphore, value_to_factor);
+			semaphore.acquire();	
+			SomethingSlow worker = new SomethingSlow(semaphore, intervals.get(i+1).get(0), intervals.get(i+1).get(1));
 			new Thread(worker).start();
+			TimeUnit.SECONDS.sleep(1);
 		}
 		
 		for(int i=0; i<num_threads; i++) {
 			semaphore.acquire();
-		}
-		System.out.println("Ellapsed Time: " + (System.currentTimeMillis()- start_time)/100f);
+		}		
+		completed = false;
+		
+		System.out.println("The total number of primes found is: " + list_of_primes.size());
+		System.out.println("Ellapsed Time: " + (System.currentTimeMillis()- start_time)/1000f);
 	}
 	
 	
